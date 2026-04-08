67b928ab9e1174791553290ea789ef3b62fffc5a
Niranjan G <67407690+NiranjanG8@users.noreply.github.com>
2026-04-09 02:03:17 +0530

    Create anomaly_detector.py

---
+++ src/anomaly_detector.py
@@ -0,0 +1,29 @@
+from sklearn.ensemble import IsolationForest
+import numpy as np
+
+def detect_anomalies(logs):
+    ip_counts = {}
+
+    for log in logs:
+        ip = log["ip"]
+        ip_counts[ip] = ip_counts.get(ip, 0) + 1
+
+    ips = list(ip_counts.keys())
+    values = np.array(list(ip_counts.values())).reshape(-1, 1)
+
+    model = IsolationForest(contamination=0.2)
+    model.fit(values)
+
+    preds = model.predict(values)
+
+    alerts = []
+    for i, pred in enumerate(preds):
+        if pred == -1:
+            alerts.append({
+                "type": "Anomaly",
+                "ip": ips[i],
+                "severity": "HIGH",
+                "message": f"Anomalous activity from {ips[i]}"
+            })
+
+    return alerts
