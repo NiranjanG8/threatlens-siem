ee3154e0356f9be33e306d68b2359bf42e41566f
Niranjan G <67407690+NiranjanG8@users.noreply.github.com>
2026-04-09 02:02:44 +0530

    Create rules.py

---
+++ src/rules.py
@@ -0,0 +1,35 @@
+from collections import defaultdict
+
+def detect_bruteforce(logs):
+    counts = defaultdict(int)
+
+    for log in logs:
+        if log["event"] == "failed_login":
+            counts[log["ip"]] += 1
+
+    return [
+        {
+            "type": "Brute Force",
+            "ip": ip,
+            "severity": "HIGH",
+            "message": f"Brute force attack from {ip}"
+        }
+        for ip, count in counts.items() if count >= 3
+    ]
+
+
+def detect_suspicious_activity(logs):
+    counts = defaultdict(int)
+
+    for log in logs:
+        counts[log["ip"]] += 1
+
+    return [
+        {
+            "type": "Suspicious Activity",
+            "ip": ip,
+            "severity": "MEDIUM",
+            "message": f"High activity from {ip}"
+        }
+        for ip, count in counts.items() if count >= 5
+    ]
